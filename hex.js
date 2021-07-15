2dead5978ce2c26b6b2656b16bd27cc75d72639c
Sahaja Nelakanti <sahajanelakanti@sahajas-mbp.myfiosgateway.com>
2021-07-15 15:37:30 -0400

    code to generate random hex value

--- hex.js
+++ hex.js
@@ -1,1 +1,12 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+btn.addEventListener("click", function () {
+  const arr = [];
+  for (let i = 0; i < 6; i++) {
+    const randomVal = hex[Math.floor(Math.random() * hex.length)];
+    arr.push(randomVal);
+  }
+  const randomHex = `#${arr.join("")}`;
+  document.body.style.backgroundColor = randomHex;
+  color.textContent = randomHex;
+});
